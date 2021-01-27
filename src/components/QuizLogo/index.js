147748aead9085634ca0bf1f10b535b55c50ad41
ERIC OLIVEIRA LOPES <TEN_LOPES@dcmun.eb.mil>
2021-01-27 21:18:31 +0400

    correção da logo

--- src/components/QuizLogo/index.js
+++ src/components/QuizLogo/index.js
@@ -1,20 +1,32 @@
 import styled from 'styled-components';
-import Image from 'next/image';
-
-import logoImg from './logo_ready.png';
 
 function Logo() {
   return (
-    <Image src={logoImg} alt="Astronomia Quiz" width={300} height={300}/>
+    <QuizLogo>
+      <span>Quiz</span>
+      <h1>ASTRONOMIA</h1>
+    </QuizLogo>
   )
 }
 
-const QuizLogo = styled(Logo)`
-  margin: auto;
+const QuizLogo = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
   display: block;
   @media screen and (max-width: 500px) {
     margin: 0;
   }
+
+  h1 {
+    font-size: 50px;
+    margin-top: -15px;
+  }
+
+  span {
+    font-size: 25px;
+    color: ${({ theme }) => theme.colors.secondary};
+  }
 `;
 
-export default QuizLogo; 
+export default Logo; 
