f5027600561cc513b3ccd1d41b1fc5b102a3dd1a
EricEOL <lopes.eric051@gmail.com>
2021-01-26 22:20:09 -0300

    atualização do input da tela inicial

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import db from '../db.json';
+import { useRouter } from 'next/router';
 
 const GlobalStyle = createGlobalStyle`
   *{
@@ -34,6 +35,9 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
           <title>Quiz de Astronomia</title>
+
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
         
           <meta name="description" content="Quiz sobre Astronomia" />
 
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,12 @@
 import styled from 'styled-components'
 import db from '../db.json';
 import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 
-/* const BackgroundImage = styled.div`
-  background-image: url(${db.bg});
-  flex: 1;
-  background-size: cover;
-  background-position: center;
-` */
+import { useRouter } from 'next/router';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -24,17 +20,39 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+
+  const router = useRouter()
+  
+  const [name, setName] = React.useState('');
+
+
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
-
+        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>The lengend of zelda</h1>
           </Widget.Header>
           
           <Widget.Content>
-            <p>ssssssssssssssssssssssssssssss</p>
+            <form onSubmit={function(event){
+              event.preventDefault();
+
+
+              router.push(`/quiz?name=${name}`);
+
+              console.log(name);
+            }}>
+
+              <input onChange={function(event){
+                setName(event.target.value);
+              }} 
+              type="name" placeholder="Preencha com seu nome"
+              />
+
+              <button type="submit" disabled={name.length === 0}>Jogar {name}</button>
+            </form>
           </Widget.Content>
 
         </Widget>
